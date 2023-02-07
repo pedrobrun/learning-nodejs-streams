5a0c2f09ad0b3f12b2f77be6a22a4532f2ec628f
Pedro Brun <pedroluccavb@gmail.com>
2023-02-07 17:07:00 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from 'express';
+const app = express();
+const PORT = 3000;
+
+app.get('/', function (req: Request, res: Response) {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT} 📚🚀`));
